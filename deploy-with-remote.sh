4c8f4213a413468bb15072e2d9734a2543ad1549
ch1aki <ch1aki@users.noreply.github.com>
2022-06-18 14:29:32 +0900

    fix

--- deploy-with-remote.sh
+++ deploy-with-remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
 repo_path=/home/isucon/isucon11-qualify
-branch={$1:-master} # default: master
+branch={$1:-main} # default: main
 
 echo 'running deploy to this server'
 git -C ${repo_path} pull origin master
@@ -14,7 +14,7 @@ echo ''
 for srv in "isu2" "isu3"
 do
   echo 'starting for '${srv}
-  ssh isucon@${srv} git -C ${repo_path} pull origin master || ssh isucon@${srv} git clone https://github.com/t2-isucon/isucon11-qualify.git
+  ssh isucon@${srv} git -C ${repo_path} pull origin main || ssh isucon@${srv} git clone https://github.com/t2-isucon/isucon11-qualify.git
   ssh isucon@${srv} git -C ${repo_path} switch ${branch}
   ssh isucon@${srv} ${repo_path}/deploy.sh
   echo 'finished for '${srv}
